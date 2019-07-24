74bb4c0c8c7931e9070e26f70b4b7eb90f89dc87
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-07-24 16:09:07 +0200

    add body parser

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const cors = require('cors');
+const Mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const express = require('express');
+const router = require('./router');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server started!')
-  })
+
+
+
+
+
+ 
+Mongoose.connect("mongodb://localhost:27017/Chat-app",{useNewUrlParser:true});
+const db = Mongoose.connection;
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
